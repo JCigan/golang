3e3a6691ab3b9931505711961223732173a9aa9b
JCigan <jdcigan@gmail.com>
2018-02-27 07:46:21 -0800

    Made second round of updates to scripts

--- go-intro/main.go
+++ go-intro/main.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-// this is a comment
-
 func main() {
-    fmt.Println("Hello, my name is Jason!")
+    fmt.Println(len("Hello, World"))
+    fmt.Println("Hello, World"[1])
+    fmt.Println("Hello, " + "World")
 }
