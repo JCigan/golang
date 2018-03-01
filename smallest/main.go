65c7634e87386b3513963cc62f18c8b694698246
JCigan <jdcigan@gmail.com>
2018-02-28 18:44:06 -0800

    Improved semantics of min script

--- smallest/main.go
+++ smallest/main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	var min int
 	x := []int{
 		48, 96, 86, 68,
 		57, 82, 63, 70,
 		37, 34, 83, 27,
 		19, 97, 9, 17,
 	}
-	var y int = x[0]
-	for _, value := range x {
-		if value < y {
-			y = value
+	for index, value := range x {
+		if index == 0 || value < min {
+			min = value
 		}
 	}
-	fmt.Println("Smallest number:", y)
+	fmt.Println("Smallest number:", min)
 }
